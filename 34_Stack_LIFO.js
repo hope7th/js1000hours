ba93bf842d6b9e25e7bc49c5bd9fdcd2b8a2aed5
zhangfeipeng <18701042121@163.com>
2019-03-25 11:16:15 +0800

    const 保存的是内存地址，对象无效

--- 34_Stack_LIFO.js
+++ 34_Stack_LIFO.js
@@ -71,10 +71,13 @@ let es6Stack = (function () {
        print() {
            let s = items.get(this);
            console.log(s.toString())
+           return s.toString();
        }
    }
    return Stack;
 })()
+
+console.log('----------------')
 
 stack = new es6Stack();
 console.log(stack.isEmpty());
@@ -85,3 +88,30 @@ stack.push(111);
 console.log(stack.size());
 console.log(stack.isEmpty());
 console.log(stack.print())
+
+console.log('3----------------')
+const tmpitems = new WeakMap();
+console.log(tmpitems)
+let dic = {ss:"ss"};
+tmpitems.set(dic,[]);
+let ssssss = tmpitems.get(dic);
+console.log(ssssss);
+ssssss.push('s1');
+ssssss.push('s2');
+ssssss.push('s3');
+console.log(ssssss);
+
+// console.log(tmpitems);
+console.log(tmpitems.get(dic));
+
+
+const tmps = new Object();
+tmps.s1='s1';
+tmps.s2='s2';
+console.log(tmps);
+
+// const实际上保证的，并不是变量的值不得改动，而是变量指向的那个内存地址不得改动。对于简单类型的数据（数值、字符串、布尔值），值就保存在变量指向的那个内存地址，因此等同于常量。
+//
+// 但对于复合类型的数据（主要是对象和数组），变量指向的内存地址，保存的只是一个指针，const只能保证这个指针是固定的，至于它指向的数据结构是不是可变的，就完全不能控制了。因此，将一个对象声明为常量必须非常小心。
+//
+
