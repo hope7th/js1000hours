2d3d357b3b8016ac9b46ce248afc31ef48d7351c
hope7th <18701042121@163.com>
2019-05-21 07:08:13 +0800

    41

--- 41_链表_link.js
+++ 41_链表_link.js
@@ -69,7 +69,28 @@ function LinkedList() {
         }
 
         return string;
-    }
+    };
+    this.indexof = function (element) {
+        let current = head;
+        index = 0;
+        while (current){
+            if (element==current.element){
+                return index
+            }
+            index++;
+            current = current.next;
+        }
+        return -1
+    };
+    this.isEmpty = function () {
+        return length === 0;
+    };
+    this.size = function () {
+        return length;
+    };
+    this.getHead = function () {
+        return head
+    };
 
 }
 
@@ -79,3 +100,89 @@ link_list.append('1');
 link_list.append('2');
 link_list.append('3');
 console.log(link_list.toString());
+//双向链表
+
+function DoublyLinkList() {
+    let node = function (element) {
+        this.element = element;
+        this.next = null;
+        this.prev = null;
+    };
+    let length = 0;
+    let head = null;
+    let tail = null;
+    this.insert = function (position,element) {
+        if (position>=0&&position<=length){
+            let node = new Node(element),
+                current = head,
+                previous,
+                index = 0;
+            if (position==0){
+                if (!head){
+                    head = node;
+                    tail = node;
+                } else {
+                    node.next = current;
+                    current.prev = node;
+                    head = node;
+                }
+            } else if (position===length){
+                current = tail;
+                node.prev = current;
+                current.next = node;
+                tail = node;
+            }else {
+                while (index++<position){
+                    previous = current;
+                    current = current.next;
+                }
+                node.prev = previous;
+                node.next = current;
+                current.prev = node;
+                previous.next = node;
+
+            }
+            length++;
+            return true;
+        } else {
+            return false;
+        }
+
+    };
+
+    this.removeAt = function (position) {
+        if (position>-1 && position<length){
+            let current = head,
+                previous,
+                index = 0;
+
+            if (position==0){
+                head = current.next;
+                if (length===1){
+                    tail= null;
+                } else {
+                    head.prev = null;
+                }
+            }else if (position===length-1){
+                current = tail;
+                tail = current.prev;
+                tail.next = null;
+            }else {
+                while (index++<position){
+                    previous = current;
+                    current = current.next;
+                }
+                previous.next = current.next;
+                current.next.prev = previous;
+            }
+
+            length--;
+            return current.element;
+        }else {
+            return null;
+        }
+    };
+}
+
+//循环链表
+//tail.next = head,head.prev = tail
