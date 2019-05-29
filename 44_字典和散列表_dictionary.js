386861fb992272f24d2b19eaf181aa50a3dc6066
hope7th <18701042121@163.com>
2019-05-29 21:54:40 +0800

    44

--- 44_字典和散列表_dictionary.js
+++ 44_字典和散列表_dictionary.js
@@ -54,3 +54,10 @@ function HashTable() {
       table[loseloseHashCode(key)] = undefined
     };
 }
+
+var hash = new HashTable();
+hash.put('Gandalf','gandalf@email.com');
+hash.put('John','john@email.com');
+hash.put('Tyrion','tyrion@email.com');
+console.log(hash.get("Gandalf"));
+console.log(hash.get("John"));
