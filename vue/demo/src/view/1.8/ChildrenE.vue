e1acc52e6898757de40d1c6472c67f41d571edef
zhangfeipeng <18701042121@163.com>
2019-06-13 18:20:03 +0800

    1.8

---
+++ vue/demo/src/view/1.8/ChildrenE.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="border2">
+        <h3 :style="{ color:theme.color }">E节点</h3>
+        <button @click="handleClick">改变color</button>
+    </div>
+    
+</template>
+
+<script>
+  export default {
+    name: 'ChildrenE',
+    inject:{
+      theme:{
+        default:()=>({})
+      }
+    },
+    methods:{
+      handleClick(){
+        if (this.theme.changeColor){
+          this.theme.changeColor("green")
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/demo/src/view/1.8/ChildrenI.vue
@@ -0,0 +1,21 @@
+<template functional>
+    <div class="border2">
+        <!--<h3 :style="{color:injections.theme.color}"> I 节点</h3>-->
+        <h3 :style="{ color:injections.theme.color }"> I 节点</h3>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'ChildrenI',
+    inject:{
+      theme:{
+        default:()=>({})
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
