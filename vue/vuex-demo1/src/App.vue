5f5207c18ddae94d5980e918b1633f23b874dda4
hope7th <18701042121@163.com>
2019-07-06 10:04:57 +0800

    0706

---
+++ vue/vuex-demo1/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+   {{count}}
+    <br>
+    {{$store.getters.doubleCount}}
+    <botton @click="$store.commit('increment')">ssss</botton>
+    <botton @click="$store.dispatch('increment')">count++</botton>
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  },
+  computed:{
+    count(){
+      return this.$store.state.count;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vuex-demo2/src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuex from './min-vuex'
+
+Vue.use(Vuex)
+Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  state:{
+    count:0
+  },
+  mutations:{
+    increment(state) {
+      state.count++
+    }
+  },
+  getters:{
+    doubleCount(state){
+      debugger
+      return state.count * 2
+    }
+  }
+})
+// Vue.prototype.$store = store;
+new Vue({
+  store,
+  render: h => h(App),
+}).$mount('#app')
