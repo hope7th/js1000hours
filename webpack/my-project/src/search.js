f62240a0fdacc28e61f9cd2007cf6116f4d4780f
hope7th <18701042121@163.com>
2019-09-02 07:18:18 +0800

    css-loader style-loader less-loader

--- webpack/my-project/src/search.js
+++ webpack/my-project/src/search.js
@@ -1,1 +1,14 @@
-document.write("hello search")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './search.css';
+import './search.less';
+class Search extends React.Component {
+    render(){
+        return <div class="search-text">Search Text</div>
+    }
+}
+
+ReactDOM.render(
+    <Search></Search>,
+    document.getElementById("root")
+)
--- webpack/my-project/webpack.config.js
+++ webpack/my-project/webpack.config.js
@@ -1,12 +1,34 @@
-const path =  require('path')
+const path = require('path')
 module.exports = {
     entry: {
-        index:'./src/index.js',
-        search:'./src/search.js'
+        index: './src/index.js',
+        search: './src/search.js'
     },
-    output:{
-        path:path.join(__dirname,'dist'),
-        filename:'[name].js'
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
     },
-    mode:'production'
+    mode: 'production',
+    module: {
+        rules: [{
+                test: /.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ]
+            },
+            {
+                test: /.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'less-loader'
+                ]
+            }
+        ]
+    }
 }
