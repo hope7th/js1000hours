062514df8462c90e954d3654cd7cc7d2a342f143
zhangfeipeng <18701042121@163.com>
2020-04-07 14:12:43 +0800

    change name

--- crosstalk-fo-js/96_(...x)：不是表达式、语句、函数，但它却能执行.js
+++ crosstalk-fo-js/96_(...x)：不是表达式、语句、函数，但它却能执行.js
@@ -52,7 +52,7 @@ try {
 }
 
 // 这个看不懂
-console.log(Object.getOwnPropertyNames(tor.constructor.prototype))
+console.log(Object.getOwnPropertyNames(tor.constructor))
 console.log(Object.getOwnPropertyNames(x2.constructor.prototype))
 
 
@@ -61,12 +61,55 @@ function foo3(x=5){
         next:()=>{
             return {done:!x,value:x&&x--}
         },
-        "return":()=>console.log("------RETUREN")
+        return:()=>console.log("------RETUREN")
     }
 }
 let x3 = new Object;
 x3[Symbol.iterator]=foo3;
 for (let i of x3){
     console.log(i);
-    // break;
+    // break; Iterator result undefined is not an object
 };
+
+
+function foo4(x=5){
+    return {
+        "next":()=> {throw new Error},
+        "throw":()=> console.log("THROW!")
+    }
+}
+let x4 = new Object;
+x4[Symbol.iterator] = foo4;
+// console.log(...x4)
+
+function foo5(x=5){
+    return {
+        next:()=>{
+            return {done:!x,value:x&&x--}
+        },
+        "return":()=>console.log("return!"),
+        "throw":()=>console.log("throw!")
+    }
+}
+let x5 = new Object;
+x5[Symbol.iterator] = foo5;
+console.log(...x5)
+/**
+ * 迭代过程并不是一个语法执行的过程，而是应该理解为一组函数执行的过程；
+ * 对于这一批函数执行过程中的结束行为，也应该理解为函数内的异常或退出。
+ */
+
+for (let i of x5) {  
+    console.log(i)
+    if (i == 2){
+       
+        // break;
+    }
+        
+}
+
+// 由于 for .. of语句将获得 x 对象的迭代变量 tor，那么它也将管理 x 对象的迭代过程。
+
+
+
+
