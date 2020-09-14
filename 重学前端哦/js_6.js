ce969e9050b1a2fbad76aa958148d296cb3eb2fa
zhangfeipeng <18701042121@163.com>
2020-09-14 09:20:28 +0800

    0914

---
+++ 重学前端哦/js_6.js
@@ -0,0 +1,31 @@
+function foo() {
+
+}
+const foo1 = () => {
+
+}
+class C {
+    foo3() {
+
+    }
+}
+
+function *foo4(){
+
+}
+
+class Foo5{
+    constructor(){
+
+    }
+}
+async function foo6(){
+
+}
+
+const foo7 = async ()=>{
+
+}
+async function *foo8(){
+
+}
