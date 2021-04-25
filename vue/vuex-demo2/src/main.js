f71a019733eddcc14a2f13e04476340c24b04990
zhangfeipeng <18701042121@163.com>
2021-04-25 17:51:16 +0800

    0425

--- vue/vuex-demo2/src/main.js
+++ vue/vuex-demo2/src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from './min-vuex'
+import Vuex from './min-vuex2'
 
 Vue.use(Vuex)
 Vue.config.productionTip = false
@@ -9,17 +9,17 @@ const store = new Vuex.Store({
   state:{
     count:0
   },
-  mutations:{
-    increment(state) {
-      state.count++
-    }
-  },
-  getters:{
-    doubleCount(state){
-      debugger
-      return state.count * 2
-    }
-  }
+  // mutations:{
+  //   increment(state) { 
+  //     state.count++
+  //   }
+  // },
+  // getters:{
+  //   doubleCount(state){
+  //     debugger
+  //     return state.count * 2
+  //   }
+  // }
 })
 // Vue.prototype.$store = store;
 new Vue({
